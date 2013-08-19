eff1de01cccc16428f4a4a5ed0f4ce16d4ec3a5e
dtchanpura <dtchanpura@gmail.com>
2013-08-19 19:51:21 +0530

    Program For Reading Image from Source and Displaying on a window

--- readimage.c
+++ readimage.c
@@ -3,5 +3,10 @@
 
 int main()
 {
-    return 0;
+	IplImage* img1 = cvLoadImage("~/images/hello.jpg",CV_LOAD_IMAGE_COR);
+    	cvNamedWindow("Display",CV_WINDOW_AUTOSIZE);
+    	cvShowImage("Display",img1);
+    	cvWaitKey(0);
+    	cvReleaseImage(&img1);
+	return 0;
 }
