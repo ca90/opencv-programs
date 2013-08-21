4b46915fb036461030a680aeeb16b9c4d0fb5e17
dtchanpura <dtchanpura@gmail.com>
2013-08-21 23:41:26 +0530

    Removed comments

--- Basics/readimage.c
+++ Basics/readimage.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-	IplImage* img1 = cvLoadImage("~/images/hello.jpg",CV_LOAD_IMAGE_COR);
-    	cvNamedWindow("Display",CV_WINDOW_AUTOSIZE);
+	IplImage* img1 = cvLoadImage("/home/darshil/images/hello.jpg", CV_LOAD_IMAGE_COLOR);
+    	cvNamedWindow("Display", CV_WINDOW_AUTOSIZE);
     	cvShowImage("Display",img1);
     	cvWaitKey(0);
     	cvReleaseImage(&img1);
