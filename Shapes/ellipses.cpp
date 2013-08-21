c5f8d3eec14aa476b3ed0eab8b402c1012729466
dtchanpura <dtchanpura@gmail.com>
2013-08-22 00:58:15 +0530

    Reformatted

--- Shapes/ellipses.cpp
+++ Shapes/ellipses.cpp
@@ -6,13 +6,12 @@ void drawEllipse(Mat, double);
 
 int main() {
 
-	Mat image= Mat::zeros(400, 400, CV_8UC3);
+	Mat image = Mat::zeros(400, 400, CV_8UC3);
 	//four ellipses with angles 180deg, 135deg, 90deg, 45deg
-	drawEllipse(image,45);
-	drawEllipse(image,90);
-	drawEllipse(image,180);
-
-	drawEllipse(image,135);
+	drawEllipse(image, 45);
+	drawEllipse(image, 90);
+	drawEllipse(image, 180);
+	drawEllipse(image, 135);
 	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
 	imshow("Display Image", image);
 
@@ -20,12 +19,11 @@ int main() {
 
 	return 0;
 }
-void drawEllipse(Mat img, double angle)
-{
+void drawEllipse(Mat img, double angle) {
 	int thickness = 2;
-		int lineType = 8;
+	int lineType = 8;
 
-		ellipse(img, Point(200, 200), Size(100, 25), angle, 0, 360,
-					Scalar(255, 0, 0), thickness, lineType);
+	ellipse(img, Point(200, 200), Size(100, 25), angle, 0, 360,
+			Scalar(255, 0, 0), thickness, lineType);
 }
 
