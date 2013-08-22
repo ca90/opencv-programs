bbe8ab4f73a3f009af76468e4e1912342c78e268
Darshil Chanpura <dtchanpura@gmail.com>
2013-08-22 18:20:01 +0530

    Added Comments for python version of readimage

--- Basics/readimage.py
+++ Basics/readimage.py
@@ -1,6 +1,6 @@
-from cv2.cv import *
-img=LoadImageM("/home/darshil/images/hello.jpg")
-NamedWindow("Display",CV_WINDOW_AUTOSIZE)
-ShowImage("Display",img)
-WaitKey(0)
+from cv2.cv import * # importing all the opencv libraries
+img=LoadImageM("/home/darshil/images/hello.jpg") # loading image in form of matrix * dont know but M will mean Matrix 
+NamedWindow("Display",CV_WINDOW_AUTOSIZE) # Defining a window named "Display" . Doesn't matter if added or not
+ShowImage("Display",img)  #Displaying the image
+WaitKey(0) #waitkey to wait till the output comes
 
