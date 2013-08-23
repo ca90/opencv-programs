96ab0666df1c0ce43b1c7efdaafba2a1e2aacd0f
dtchanpura <dtchanpura@gmail.com>
2013-08-23 22:41:59 +0530

    Added shponimg.cpp, a C++ variant of shponimg.py

---
+++ Shapes/shponimg.cpp
@@ -0,0 +1,41 @@
+#include<cv.h>
+#include<highgui.h>
+
+using namespace cv;
+void drawEllipse(Mat, double);
+void drawCircle(Mat);
+int main() {
+
+	Mat image = imread("/home/darshil/images/smaller.jpg");
+
+	Scalar color1;
+	//four ellipses with angles 180deg, 135deg, 90deg, 45deg
+	drawEllipse(image, 45);
+	drawEllipse(image, 90);
+	drawEllipse(image, 180);
+	drawEllipse(image, 135);
+	drawCircle(image);
+	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+	imshow("Display Image", image);
+	imwrite( "/home/darshil/workspace/opennew1/src/images/Ellipses_Image.jpg",image );
+	waitKey(0);
+
+	return 0;
+}
+void drawEllipse(Mat img, double angle) {
+	int thickness = 2;
+	int lineType = 8;
+
+	ellipse(img, Point(200, 200), Size(100, 25), angle, 0, 360,
+			Scalar(255, 0, 0), thickness, lineType);
+}
+
+void drawCircle(Mat img)
+{
+	int thickness = -1;
+	int lineType = 8;
+	circle(img, Point(200,200), 50, Scalar(255,255,200), thickness, lineType);
+
+}
+
+
