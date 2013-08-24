dab45fd1091e258c07dac40fbe28b40128706a02
dtchanpura <dtchanpura@gmail.com>
2013-08-24 21:13:08 +0530

    Editted polygon.cpp, Drawing polygons on any image

--- Shapes/polygon.cpp
+++ Shapes/polygon.cpp
@@ -2,19 +2,20 @@
 #include <highgui.h>
 #include <math.h>
 using namespace cv;
+using namespace std;
 int main() {
 
-	Mat img1=imread ("/home/images/smaller.jpg");
-	Point p[1][5];
-	int a, b;
-	for (int i = 0; i < 5; i++) {
-		a = sin(i);
-		b = cos(i);
-		p[0][i] = Point(50 * (1 + a), 50 * (1 + b));
-	}
-	const Point* ppt[1] = { p[0] };
-	int npt[] = { 20 };
+	Mat img1=imread ("/home/darshil/images/smaller.jpg");
+	Point p[1][6];
+	p[0][0]=Point(30,30);
+	p[0][1]=Point(30,60);
+	p[0][2]=Point(60,60);
+	p[0][3]=Point(60,120);
+	p[0][4]=Point(120,120);
+	p[0][5]=Point(120,30);
 
+	const Point* ppt[1] = { p[0] };
+	int npt[] = { 6 };
 	fillPoly(img1, ppt, npt, 1, Scalar(255, 255, 255), 8);
 	imshow("window",img1);
 	waitKey(0);
