737cc04249308f68d26efc50607b6a5f4304af0d
dtchanpura <dtchanpura@gmail.com>
2013-08-25 23:03:26 +0530

    Issue: How to use arrays in python

--- Shapes/polygon.py
+++ Shapes/polygon.py
@@ -1,7 +1,7 @@
 from cv2.cv import *
 from numpy import * 
 img=LoadImageM("/home/darshil/images/smaller.jpg")
-polys=array(cv.Point,[(50,50),(100,50),(100,100),(50,100)])
+polys=array(cv.Point,[(50,50),(100,50),(100,100),(50,100)]) # how to generate array
 FillPoly(img, polys, (255,255,255) )
 ShowImage("Window",img) # Displaying the image ... 
 print img.rows
