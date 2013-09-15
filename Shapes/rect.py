4935c354bc5f95daadc642fde00daa49377fb1ae
dtchanpura <dtchanpura@gmail.com>
2013-09-15 11:35:56 +0530

    Updated rect.py added comments

--- Shapes/rect.py
+++ Shapes/rect.py
@@ -1,6 +1,6 @@
 from cv2.cv import * # importing all the opencv libraries
 img=CreateMat(600,600,CV_8UC3) # creating blank image of 400x400 pixels
-Rectangle(img,(img.rows/2,img.cols/3),(img.rows/3,img.cols/2),(255,255,255), 3) # Rectangle(img, center, radius, color [, thickness [, lineType [, shift]]])
+Rectangle(img,(img.rows/2,img.cols/3),(img.rows/3,img.cols/2),(255,255,255), 3) # Rectangle(img, pt1, pt2, color [, thickness [, lineType [, shift]]])
 ShowImage("Display",img) # Displaying the image ... 
 WaitKey(0)
 
