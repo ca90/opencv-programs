1a22f1c4875d48a686a95f2fbb8014e798f1fc6e
Darshil Chanpura <opensourcelabs@gmail.com>
2013-09-19 11:48:13 +0530

    Update readimage.c

--- Basics/readimage.c
+++ Basics/readimage.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-	IplImage* img1 = cvLoadImage("/home/darshil/images/hello.jpg", CV_LOAD_IMAGE_COLOR);
-    	cvNamedWindow("Display", CV_WINDOW_AUTOSIZE);
-    	cvShowImage("Display",img1);
+	IplImage* img1 = cvLoadImage("~/images/hello.jpg", CV_LOAD_IMAGE_COLOR);//loading image to variable pointer img1
+    	cvNamedWindow("Display", CV_WINDOW_AUTOSIZE);//creating a window named 'Display'
+    	cvShowImage("Display",img1);//showing the image in window
     	cvWaitKey(0);
     	cvReleaseImage(&img1);
 	return 0;
